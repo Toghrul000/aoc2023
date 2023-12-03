82500eceabdd6ccd36398a1c2b3ab150f4fc02b0
Toghrul000 <togrul.garalov@gmail.com>
2023-12-03 03:06:11 +0100

    day2 done

--- src/bin/day2/main.rs
+++ src/bin/day2/main.rs
@@ -2,16 +2,95 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-fn main() {
-    // File hosts.txt must exist in the current path
+#[allow(dead_code)]
+fn part1()->i32 {
+    let mut sum = 0;
+
     if let Ok(lines) = read_lines("./inputs/input_day2.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(game_line) = line {
-                println!("{}", game_line);
+                let mut parts = game_line.split(":");
+                let game_id = parts.next().unwrap().trim().split(" ").nth(1).unwrap().parse::<i32>().unwrap();
+                let sets_string = parts.next().unwrap().trim().split(";");
+                let mut pass = true;
+                for set in sets_string {
+                    let mut total_red = 0;
+                    let mut total_blue = 0;
+                    let mut total_green = 0;
+                    let pairs: Vec<&str> = set.trim().split(", ").collect();
+                    for pair in pairs {
+                        let mut cubes = pair.split(" ");
+                        let number = cubes.next().unwrap().trim().parse::<i32>().unwrap();
+                        let color = cubes.next().unwrap().trim();
+                        match color {
+                            "red" => total_red += number,
+                            "blue" => total_blue += number,
+                            "green" => total_green += number,
+                            _ => (),
+                        }
+                    }
+                    
+                    if total_red > 12 || total_green > 13 || total_blue > 14 {
+                        pass = false;
+                    }
+
+                }
+                if pass {
+                    sum+=game_id;
+                }
+
+                
             }
         }
     }
+    println!("Sum is {}", sum);
+    sum
+
+}
+
+#[allow(unused)]
+fn main() {
+
+    let mut sum = 0;
+
+    if let Ok(lines) = read_lines("./inputs/input_day2.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+
+
+            let mut game_id = 0;
+            let mut max_red = 0;
+            let mut max_blue = 0;
+            let mut max_green = 0;
+    
+            if let Ok(game_line) = line {
+                let mut parts = game_line.split(":");
+                let game_id = parts.next().unwrap().trim().split(" ").nth(1).unwrap().parse::<i32>().unwrap();
+                let sets_string = parts.next().unwrap().trim().split(";");
+                for set in sets_string {
+                    let pairs: Vec<&str> = set.trim().split(", ").collect();
+                    for pair in pairs {
+                        let mut cubes = pair.split(" ");
+                        let number = cubes.next().unwrap().trim().parse::<i32>().unwrap();
+                        let color = cubes.next().unwrap().trim();
+                        match color {
+                            "red" => if number > max_red { max_red = number },
+                            "blue" => if number > max_blue { max_blue = number },
+                            "green" => if number > max_green { max_green = number },
+                            _ => (),
+                        }
+                    }
+                }
+                let power = max_red * max_green * max_blue;
+                sum += power;
+
+
+                
+            }
+        }
+    }
+    println!("Sum for part2 is {}", sum);
 }
 
 // The output is wrapped in a Result to allow matching on errors
